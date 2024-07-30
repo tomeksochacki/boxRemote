65dac11febad6306b7cc4d9f85ed236c7c861249
Tomek <tomeksochacki@wp.pl>
2024-07-30 13:07:44 +0200

    newBox

---
+++ src/main/java/Box.java
@@ -0,0 +1,11 @@
+public class Box {
+    double width;
+    double height;
+    double depth;
+
+    public Box(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+}
