8d7f5071cf6b831c5d822c747323189695f39d25
Tomek <tomeksochacki@wp.pl>
2024-07-30 14:08:03 +0200

    changing the Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args){
+        Box myBox = new Box(3.2, 3.0,3.0 );
+        Gson gson = new Gson();
+        String json = gson.toJson(myBox);
+        System.out.println(json);
+    }
+
+}
