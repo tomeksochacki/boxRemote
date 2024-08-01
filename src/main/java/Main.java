ce30f624e24574d572ac4e91a2089714dc84ab03
Tomek <tomeksochacki@wp.pl>
2024-08-01 12:53:58 +0200

    Change from row to aow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("ddd");
         System.out.println("ddd");
         System.out.print("Row");
-        System.out.print("Row");
+        System.out.print("Aow");
     }
 
 }
